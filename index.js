6ff48e28cdf192afab4ee332649db37b523a1da9
Ansel Bridgewater <iamansel@gmail.com>
2019-07-15 21:36:42 -0700

    Added comments for what each method does

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
+// Comments borrowed from Mitchell Hudson, @soggybag
+
+// makes the first character of a given string uppercase
 String.prototype.capitalize = function() {
 
 }
 
+// makes all characters uppercase
+// this is the same as .toUppercase()
 String.prototype.allCaps = function() {
 
 }
 
+// makes the first character of each word uppercase
+// imagine that each word is separated by a space
 String.prototype.capitalizeWords = function() {
 
 }
 
+// makes all odd characters uppercase and even characters lowercase
 String.prototype.oddCaps = function() {
 
 }
 
+// removes all spaces from the beginning and end of a String along with any extra spaces in the middle
+// if more than one space appears in the middle of a string it is replaced by a single space
 String.prototype.removeExtraSpaces = function() {
 
 }
 
+// removes extra spaces and replaces spaces with the hyphen "-", and makes all characters lowercase
 String.prototype.kabobCase = function() {
 
 }
 
+// removes extra space and replaces spaces with an underscore "_", and makes all characters lowercase
 String.prototype.snakeCase = function() {
 
 }
 
+// lowercases the first character of the first word
+// then uppercases the first character of all other words, and removes all spaces
 String.prototype.camelCase = function() {
 
 }
