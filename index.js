f95251a4b008a3a0987224307903676434c14246
Ansel Bridgewater <iamansel@gmail.com>
2019-07-15 21:27:40 -0700

    Stubbed out methods that will be written

---
+++ index.js
@@ -0,0 +1,31 @@
+String.prototype.capitalize = function() {
+
+}
+
+String.prototype.allCaps = function() {
+
+}
+
+String.prototype.capitalizeWords = function() {
+
+}
+
+String.prototype.oddCaps = function() {
+
+}
+
+String.prototype.removeExtraSpaces = function() {
+
+}
+
+String.prototype.kabobCase = function() {
+
+}
+
+String.prototype.snakeCase = function() {
+
+}
+
+String.prototype.camelCase = function() {
+
+}
