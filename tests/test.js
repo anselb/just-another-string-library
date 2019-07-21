485b8f2a3363c9389c4165447c6caf2a518ca24a
Ansel Bridgewater <iamansel@gmail.com>
2019-07-20 17:24:35 -0700

    Fixed linter errors

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,9 @@
-require("../index")
+require('../index');
 
 test('Sanity check', () => {
-  expect(2 + 2).toBe(4)
-})
+  expect(2 + 2).toBe(4);
+});
 
 test('Test capitalize', () => {
-  expect("hello world".capitalize()).toBe("Hello world")
-})
+  expect('hello world'.capitalize()).toBe('Hello world');
+});
