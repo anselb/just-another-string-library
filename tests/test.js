c29e739311683b34a06ed05c9da01993affae275
Ansel Bridgewater <iamansel@gmail.com>
2019-07-20 17:43:32 -0700

    Added more tests for capitalize

--- tests/test.js
+++ tests/test.js
@@ -8,4 +8,6 @@ test('Test capitalize', () => {
   expect('hello world'.capitalize()).toBe('Hello world');
   expect(''.capitalize()).toBe('');
   expect('1hello world'.capitalize()).toBe('1hello world');
+  expect('áéíóú'.capitalize()).toBe('Áéíóú');
+  expect('49302'.capitalize()).toBe('49302');
 });
