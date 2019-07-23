ac0a485c44f8b3f2304e67c961991a567ec37d3f
Ansel Bridgewater <iamansel@gmail.com>
2019-07-23 15:54:19 -0700

    Installed babel in import functions into jest

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
 // Comments borrowed from Mitchell Hudson, @soggybag
 
 // makes the first character of a given string uppercase
-export function capitalize() {
+export function capitalize(str) {
   // Code taken from dr.dimitru in this Stack Overflow comment:
   // https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript#comment56095910_1026087
-  if (this.length === 0) return '';
-  return this[0].toUpperCase() + this.substring(1);
+  if (str.length === 0) return '';
+  return str[0].toUpperCase() + str.substring(1);
 }
 
 // makes all characters uppercase
 // this is the same as .toUppercase()
-export function allCaps() {
-  return this.toUpperCase();
+export function allCaps(str) {
+  return str.toUpperCase();
 }
 
 // makes the first character of each word uppercase
 // imagine that each word is separated by a space
-export function capitalizeWords() {
-  const words = this.split(' ');
+export function capitalizeWords(str) {
+  const words = str.split(' ');
 
   for (let i = 0; i < words.length; i += 1) {
-    words[i] = words[i].capitalize();
+    words[i] = capitalize(words[i]);
   }
 
   return words.join(' ');
 }
 
 // makes all odd characters uppercase and even characters lowercase
-export function oddCaps() {
-  const characters = this.split('');
+export function oddCaps(str) {
+  const characters = str.split('');
 
   for (let i = 0; i < characters.length; i += 1) {
     if (i % 2 === 1) {
@@ -42,8 +42,8 @@ export function oddCaps() {
 }
 
 // makes all even characters uppercase and odd characters lowercase
-export function evenCaps() {
-  const characters = this.split('');
+export function evenCaps(str) {
+  const characters = str.split('');
 
   for (let i = 0; i < characters.length; i += 1) {
     if (i % 2 === 0) {
@@ -59,8 +59,8 @@ export function evenCaps() {
 // trims all spaces from the beginning and end of a String
 // removes any extra spaces in the middle
 // if more than one space appears in the middle of a string it is replaced by a single space
-export function removeExtraSpaces() {
-  const trimString = this.trim();
+export function removeExtraSpaces(str) {
+  const trimString = str.trim();
   const items = trimString.split(' ');
   const words = [];
 
@@ -75,28 +75,28 @@ export function removeExtraSpaces() {
 
 // removes extra spaces and replaces spaces with the hyphen '-'
 // and makes all characters lowercase
-export function kabobCase() {
-  const cleanString = this.removeExtraSpaces();
+export function kabobCase(str) {
+  const cleanString = removeExtraSpaces(str);
   const words = cleanString.split(' ');
   return words.join('-').toLowerCase();
 }
 
 // removes extra space and replaces spaces with an underscore '_'
 // and makes all characters lowercase
-export function snakeCase() {
-  const cleanString = this.removeExtraSpaces();
+export function snakeCase(str) {
+  const cleanString = removeExtraSpaces(str);
   const words = cleanString.split(' ');
   return words.join('_').toLowerCase();
 }
 
 // lowercases the first character of the first word
 // then uppercases the first character of all other words, and removes all spaces
-export function camelCase() {
-  const cleanString = this.removeExtraSpaces().toLowerCase();
+export function camelCase(str) {
+  const cleanString = removeExtraSpaces(str).toLowerCase();
   const words = cleanString.split(' ');
 
   for (let i = 1; i < words.length; i += 1) {
-    words[i] = words[i].capitalize();
+    words[i] = capitalize(words[i]);
   }
 
   return words.join('');
--- tests/test.js
+++ tests/test.js
@@ -1,78 +1,88 @@
-require('../index');
+import {
+  capitalize,
+  allCaps,
+  capitalizeWords,
+  oddCaps,
+  evenCaps,
+  removeExtraSpaces,
+  kabobCase,
+  snakeCase,
+  camelCase,
+} from '../src/index';
 
 test('Sanity check', () => {
   expect(2 + 2).toBe(4);
 });
 
 test('Test capitalize', () => {
-  expect('hello world'.capitalize()).toBe('Hello world');
-  expect(''.capitalize()).toBe('');
-  expect('1hello world'.capitalize()).toBe('1hello world');
-  expect('áéíóú'.capitalize()).toBe('Áéíóú');
-  expect('49302'.capitalize()).toBe('49302');
+  expect(capitalize('hello world')).toBe('Hello world');
+  expect(capitalize('')).toBe('');
+  expect(capitalize('1hello world')).toBe('1hello world');
+  expect(capitalize('áéíóú')).toBe('Áéíóú');
+  expect(capitalize('49302')).toBe('49302');
 });
 
 test('Test allCaps', () => {
-  expect('hello world'.allCaps()).toBe('HELLO WORLD');
-  expect(''.allCaps()).toBe('');
-  expect('1hello world'.allCaps()).toBe('1HELLO WORLD');
-  expect('áéíóú'.allCaps()).toBe('ÁÉÍÓÚ');
-  expect('49302'.allCaps()).toBe('49302');
+  expect(allCaps('hello world')).toBe('HELLO WORLD');
+  expect(allCaps('')).toBe('');
+  expect(allCaps('1hello world')).toBe('1HELLO WORLD');
+  expect(allCaps('áéíóú')).toBe('ÁÉÍÓÚ');
+  expect(allCaps('49302')).toBe('49302');
 });
 
 test('Test capitalizeWords', () => {
-  expect('hello world'.capitalizeWords()).toBe('Hello World');
-  expect(''.capitalizeWords()).toBe('');
-  expect('1hello world'.capitalizeWords()).toBe('1hello World');
-  expect('áé í óú'.capitalizeWords()).toBe('Áé Í Óú');
-  expect('49302'.capitalizeWords()).toBe('49302');
+  expect(capitalizeWords('hello world')).toBe('Hello World');
+  expect(capitalizeWords('')).toBe('');
+  expect(capitalizeWords('1hello world')).toBe('1hello World');
+  expect(capitalizeWords('áé í óú')).toBe('Áé Í Óú');
+  expect(capitalizeWords('49302')).toBe('49302');
 });
 
 test('Test oddCaps', () => {
-  expect('hello world'.oddCaps()).toBe('hElLo wOrLd');
-  expect(''.oddCaps()).toBe('');
-  expect('1hello world'.oddCaps()).toBe('1HeLlO WoRlD');
-  expect('áé í óú'.oddCaps()).toBe('áÉ Í Óú');
-  expect('49302'.oddCaps()).toBe('49302');
+  expect(oddCaps('hello world')).toBe('hElLo wOrLd');
+  expect(oddCaps('')).toBe('');
+  expect(oddCaps('1hello world')).toBe('1HeLlO WoRlD');
+  expect(oddCaps('áé í óú')).toBe('áÉ Í Óú');
+  expect(oddCaps('49302')).toBe('49302');
 });
 
 test('Test evenCaps', () => {
-  expect('hello world'.evenCaps()).toBe('HeLlO WoRlD');
-  expect(''.evenCaps()).toBe('');
-  expect('1hello world'.evenCaps()).toBe('1hElLo wOrLd');
-  expect('áé í óú'.evenCaps()).toBe('Áé í óÚ');
-  expect('49302'.evenCaps()).toBe('49302');
+  expect(evenCaps('hello world')).toBe('HeLlO WoRlD');
+  expect(evenCaps('')).toBe('');
+  expect(evenCaps('1hello world')).toBe('1hElLo wOrLd');
+  expect(evenCaps('áé í óú')).toBe('Áé í óÚ');
+  expect(evenCaps('49302')).toBe('49302');
 });
 
 test('Test removeExtraSpaces', () => {
-  expect('     hello      world   '.removeExtraSpaces()).toBe('hello world');
-  expect(''.removeExtraSpaces()).toBe('');
-  expect('  1hello              world  '.removeExtraSpaces()).toBe('1hello world');
-  expect('áé    í óú  '.removeExtraSpaces()).toBe('áé í óú');
-  expect('  493 02    '.removeExtraSpaces()).toBe('493 02');
+  expect(removeExtraSpaces('     hello      world   ')).toBe('hello world');
+  expect(removeExtraSpaces('')).toBe('');
+  expect(removeExtraSpaces('  1hello              world  ')).toBe('1hello world');
+  expect(removeExtraSpaces('áé    í óú  ')).toBe('áé í óú');
+  expect(removeExtraSpaces('  493 02    ')).toBe('493 02');
 });
 
 test('Test kabobCase', () => {
-  expect('     HellO      WoRLd   '.kabobCase()).toBe('hello-world');
-  expect(''.kabobCase()).toBe('');
-  expect('  1HELLO              world  '.kabobCase()).toBe('1hello-world');
-  expect('áé    í ÓÚ  '.kabobCase()).toBe('áé-í-óú');
-  expect('  493 02    '.kabobCase()).toBe('493-02');
+  expect(kabobCase('     HellO      WoRLd   ')).toBe('hello-world');
+  expect(kabobCase('')).toBe('');
+  expect(kabobCase('  1HELLO              world  ')).toBe('1hello-world');
+  expect(kabobCase('áé    í ÓÚ  ')).toBe('áé-í-óú');
+  expect(kabobCase('  493 02    ')).toBe('493-02');
 });
 
 test('Test snakeCase', () => {
-  expect('     HellO      WoRLd   '.snakeCase()).toBe('hello_world');
-  expect(''.snakeCase()).toBe('');
-  expect('  1HELLO              world  '.snakeCase()).toBe('1hello_world');
-  expect('áé    í ÓÚ  '.snakeCase()).toBe('áé_í_óú');
-  expect('  493 02    '.snakeCase()).toBe('493_02');
+  expect(snakeCase('     HellO      WoRLd   ')).toBe('hello_world');
+  expect(snakeCase('')).toBe('');
+  expect(snakeCase('  1HELLO              world  ')).toBe('1hello_world');
+  expect(snakeCase('áé    í ÓÚ  ')).toBe('áé_í_óú');
+  expect(snakeCase('  493 02    ')).toBe('493_02');
 });
 
 
 test('Test camelCase', () => {
-  expect('     HellO      WoRLd   '.camelCase()).toBe('helloWorld');
-  expect(''.camelCase()).toBe('');
-  expect('  1HELLO              world  '.camelCase()).toBe('1helloWorld');
-  expect('áé    í ÓÚ  '.camelCase()).toBe('áéÍÓú');
-  expect('  493 02    '.camelCase()).toBe('49302');
+  expect(camelCase('     HellO      WoRLd   ')).toBe('helloWorld');
+  expect(camelCase('')).toBe('');
+  expect(camelCase('  1HELLO              world  ')).toBe('1helloWorld');
+  expect(camelCase('áé    í ÓÚ  ')).toBe('áéÍÓú');
+  expect(camelCase('  493 02    ')).toBe('49302');
 });
